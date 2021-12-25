0fd30208d3e88c44c76a637217f3c62d0f9c321e
TelepathicGrunt <alexsarnese@gmail.com>
2021-12-25 13:35:59 -0500

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+### **(V.41.0.0 Changes) (1.18.1 Minecraft)**
+
+##### Misc:
+First release.
---
+++ src/main/java/com/telepathicgrunt/featurenbtdeadlockbegone/FeatureNBTDeadlockBeGoneMain.java
@@ -0,0 +1,10 @@
+package com.telepathicgrunt.featurenbtdeadlockbegone;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(FeatureNBTDeadlockBeGoneMain.MODID)
+public class FeatureNBTDeadlockBeGoneMain {
+
+    public static final String MODID = "feature_nbt_deadlock_be_gone";
+    public FeatureNBTDeadlockBeGoneMain() {}
+}
